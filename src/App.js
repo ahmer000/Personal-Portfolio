2cc0408664b4574ad6f64ac1961f6771cdd1e09c
ahmer000 <thisisahmer000@gmail.com>
2023-08-02 09:34:36 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+
+import './App.css';
+import LoginScreen from './components/Login';
+
+
+
+
+function App() {
+  return (
+    <div className="css">
+      <LoginScreen
+        Username={'Username'}
+        header={'LOGIN'}
+        Password={'Password'}
+        RememberMe={'Remember Me'}
+        forget='Forget Password?'
+        buttontitle={'Login'}
+        SignUp={'Sign Up'}
+        userMailchanges={(e)=>{console.log(
+        e.target.value
+        );}}
+          UserPasswordChanges={(f)=>{console.log(
+            f.target.value
+          );}}
+      />
+    </div>
+   
+    
+  );
+}
+export default App;
